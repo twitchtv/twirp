48461ab9376a62ab9a8002422b86b069b7d91dad
Dave Polansky <dpolans@justin.tv>
2019-02-14 14:22:43 -0800

    update license dates to 2019

--- internal/twirptest/snake_case_names/compile_test.go
+++ internal/twirptest/snake_case_names/compile_test.go
@@ -1,4 +1,4 @@
-// Copyright 2018 Twitch Interactive, Inc.  All Rights Reserved.
+// Copyright 2019 Twitch Interactive, Inc.  All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License"). You may not
 // use this file except in compliance with the License. A copy of the License is

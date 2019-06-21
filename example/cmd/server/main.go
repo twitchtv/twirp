4bb7315ce468a2b26e8ef9c748cc91f8d4cd8ef5
Naomi B <bancron@users.noreply.github.com>
2019-06-21 10:18:01 -0700

    Fix use of rand.Intn
    The example uses rand.Intn(4) on a slice of len 5, meaning the last element (e.g. "blue") can never be returned. rand.Intn should be called with the len of the slice.

--- example/cmd/server/main.go
+++ example/cmd/server/main.go
@@ -31,10 +31,12 @@ func (h *randomHaberdasher) MakeHat(ctx context.Context, size *example.Size) (*e
 	if size.Inches <= 0 {
 		return nil, twirp.InvalidArgumentError("Inches", "I can't make a hat that small!")
 	}
-	return &example.Hat{
+	colors := []string{"white", "black", "brown", "red", "blue"}
+	names := []string{"bowler", "baseball cap", "top hat", "derby"}
+	return &haberdasher.Hat{
 		Size:  size.Inches,
-		Color: []string{"white", "black", "brown", "red", "blue"}[rand.Intn(4)],
-		Name:  []string{"bowler", "baseball cap", "top hat", "derby"}[rand.Intn(3)],
+		Color: colors[rand.Intn(len(colors))],
+		Name:  names[rand.Intn(len(names))],
 	}, nil
 }
 

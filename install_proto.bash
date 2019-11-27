151ff1b7b32040ac1d296edb1572024cd6009877
Iheanyi Ekechukwu <iekechukwu@gmail.com>
2019-11-27 18:40:27 -0500

    Upgrade protoc to 3.10.1

--- install_proto.bash
+++ install_proto.bash
@@ -18,10 +18,10 @@ PROTOC_EXISTS=$?
 if [ $PROTOC_EXISTS -eq 0 ]; then
     echo "Protoc already installed"
 	PROTOC_VERSION=`protoc --version`
-	if [ "$PROTOC_VERSION" == "libprotoc 3.5.1" ]; then
+	if [ "$PROTOC_VERSION" == "libprotoc 3.10.1" ]; then
 		exit 0
 	fi
-	echo "libprotoc 3.5.1 required, but found: $PROTOC_VERSION"
+	echo "libprotoc 3.10.1 required, but found: $PROTOC_VERSION"
 	exit 1
 fi
 
@@ -30,8 +30,8 @@ if [ "$(uname)" == "Darwin" ]; then
 elif [ `whoami` == "root" ]; then
     mkdir -p /usr/local/src/protoc
     pushd /usr/local/src/protoc
-    wget https://github.com/google/protobuf/releases/download/v3.5.1/protoc-3.5.1-linux-x86_64.zip -O /usr/local/src/protoc-3.5.1-linux-x86_64.zip
-    unzip -x ../protoc-3.5.1-linux-x86_64.zip
+    wget https://github.com/google/protobuf/releases/download/v3.10.1/protoc-3.10.1-linux-x86_64.zip -O /usr/local/src/protoc-3.10.1-linux-x86_64.zip
+    unzip -x ../protoc-3.10.1-linux-x86_64.zip
     if [ ! -e /usr/local/bin/protoc ]; then
         ln -s `pwd`/bin/protoc /usr/local/bin/protoc
     fi
@@ -41,8 +41,8 @@ elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     sudo chmod a+w /usr/local/src
     mkdir -p /usr/local/src/protoc
     pushd /usr/local/src/protoc
-    wget https://github.com/google/protobuf/releases/download/v3.5.1/protoc-3.5.1-linux-x86_64.zip -O /usr/local/src/protoc-3.5.1-linux-x86_64.zip
-    unzip -x ../protoc-3.5.1-linux-x86_64.zip
+    wget https://github.com/google/protobuf/releases/download/v3.10.1/protoc-3.10.1-linux-x86_64.zip -O /usr/local/src/protoc-3.10.1-linux-x86_64.zip
+    unzip -x ../protoc-3.10.1-linux-x86_64.zip
     if [ ! -e /usr/local/bin/protoc ]; then
         sudo ln -s `pwd`/bin/protoc /usr/local/bin/protoc
     fi

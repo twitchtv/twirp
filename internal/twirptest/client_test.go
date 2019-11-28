a25b068396f6c96410b5e9a4974ab0e54f5d7bb6
Iheanyi Ekechukwu <iekechukwu@gmail.com>
2019-11-27 19:25:46 -0500

    Add in tests to ensure hooks are working as expected.

--- internal/twirptest/client_test.go
+++ internal/twirptest/client_test.go
@@ -257,6 +257,93 @@ func TestClientRedirectError(t *testing.T) {
 	})
 }
 
+func TestClientWithHooks(t *testing.T) {
+	requestPreparedCalled := false
+	requestFinishedCalled := false
+	errorCalled := false
+
+	hooks := &twirp.ClientHooks{
+		RequestPrepared: func(ctx context.Context, req *http.Request) (context.Context, error) {
+			requestPreparedCalled = true
+			return ctx, nil
+		},
+		RequestFinished: func(ctx context.Context) {
+			requestFinishedCalled = true
+		},
+		Error: func(ctx context.Context, err twirp.Error) context.Context {
+			errorCalled = true
+			return ctx
+		},
+	}
+
+	h := PickyHatmaker(1)
+	s := httptest.NewServer(NewHaberdasherServer(h, nil))
+	defer s.Close()
+
+	// Clients
+	protoCli := NewHaberdasherProtobufClient(s.URL, &http.Client{}, twirp.WithClientHooks(hooks))
+	jsonCli := NewHaberdasherJSONClient(s.URL, &http.Client{}, twirp.WithClientHooks(hooks))
+	ctx := context.Background()
+	var resp *Hat
+	var err error
+
+	// Test proto success
+	resp, err = protoCli.MakeHat(ctx, &Size{Inches: 1})
+	if err != nil {
+		t.Fatalf("Proto client method returned unexpected error: %s", err)
+	}
+	if resp == nil {
+		t.Fatalf("Proto client method expected to return non-nil response, but it is nil")
+	}
+
+	if requestPreparedCalled == false || requestFinishedCalled == false {
+		t.Fatalf("expected both requestPreparedCalled and requestFinishedCalled to be true")
+	}
+
+	// Test proto failure
+	resp, err = protoCli.MakeHat(ctx, &Size{Inches: 666})
+	if err == nil {
+		t.Fatalf("Proto client method expected to fail, but error is nil")
+	}
+	if resp != nil {
+		t.Fatalf("Proto client method expected to return nil response on error, but returned non-nil")
+	}
+
+	if errorCalled == false {
+		t.Fatalf("expected errorCalled to be true")
+	}
+
+	requestPreparedCalled = false
+	requestFinishedCalled = false
+	errorCalled = false
+
+	// Test json success
+	resp, err = jsonCli.MakeHat(ctx, &Size{Inches: 1})
+	if err != nil {
+		t.Fatalf("JSON client method returned unexpected error: %s", err)
+	}
+	if resp == nil {
+		t.Fatalf("JSON client method expected to return non-nil response, but it is nil")
+	}
+
+	if requestPreparedCalled == false || requestFinishedCalled == false {
+		t.Fatalf("expected both requestPreparedCalled and requestFinishedCalled to be true")
+	}
+
+	// Test json failure
+	resp, err = jsonCli.MakeHat(ctx, &Size{Inches: 666})
+	if err == nil {
+		t.Fatalf("JSON client method expected to fail, but error is nil")
+	}
+	if resp != nil {
+		t.Fatalf("JSON client method expected to return nil response on error, but returned non-nil")
+	}
+
+	if errorCalled == false {
+		t.Fatalf("expected errorCalled to be true")
+	}
+}
+
 func TestClientIntermediaryErrors(t *testing.T) {
 	testcase := func(body string, code int, expectedErrorCode twirp.ErrorCode, clientMaker func(string, HTTPClient, ...twirp.ClientOption) Haberdasher) func(*testing.T) {
 		return func(t *testing.T) {
